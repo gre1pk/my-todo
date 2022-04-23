01bdf56177b9c88ffc9af5325790c692463fe8b8
gre1pk <raik1996@gmail.com>
2022-04-23 09:52:17 +0300

    feat: input timer

--- src/components/new-task-form/new-task-form.js
+++ src/components/new-task-form/new-task-form.js
@@ -7,6 +7,8 @@ export default class NewTaskForm extends React.Component {
     super(props)
     this.state = {
       label: '',
+      sec: '',
+      min: '',
     }
     this.regular = /^^\s*$/
   }
@@ -17,30 +19,63 @@ export default class NewTaskForm extends React.Component {
     })
   }
 
+  onTimerChange = ({ target }) => {
+    const { name, value } = target
+    if (value < 60) {
+      this.setState({
+        [name]: value.replace(/\D/, ''),
+      })
+    }
+  }
+
   onSubmit = (e) => {
-    const { label } = this.state
-    const { onNewTask } = this.props
     e.preventDefault()
+    const { label, sec, min } = this.state
+    const { onNewTask } = this.props
     const invalidText = this.regular.test(label)
     if (!invalidText) {
-      onNewTask(label)
+      const countSec = min * 60 + +sec
+      onNewTask(label, countSec)
       this.setState({
         label: '',
+        sec: '',
+        min: '',
       })
     }
   }
 
   render() {
-    const { label } = this.state
+    const { label, sec, min } = this.state
     return (
-      <form onSubmit={this.onSubmit}>
+      <form className="new-todo-form" onSubmit={this.onSubmit}>
         <input
           type="text"
           className="new-todo"
+          name="label"
           placeholder="What needs to be done?"
           onChange={this.onLabelChange}
           value={label}
+          maxLength="15"
         />
+        <input
+          type="text"
+          className="new-todo-form__timer"
+          name="min"
+          placeholder="Min"
+          onChange={this.onTimerChange}
+          value={min}
+          maxLength="2"
+        />
+        <input
+          type="text"
+          className="new-todo-form__timer"
+          name="sec"
+          placeholder="Sec"
+          onChange={this.onTimerChange}
+          value={sec}
+          maxLength="2"
+        />
+        <button type="submit" aria-label="submit" />
       </form>
     )
   }
